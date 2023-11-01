456b0f56aad0bfdb479af80efea60012a537970d
William <ia.williamrosales@ufg.edu.sv>
2023-10-31 19:33:38 -0600

    Agregar archivo .gitattributes para manejar finales de línea

---
+++ cine/src/main/java/sv/edu/sufg/cine/CineApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.sufg.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineApplication.class, args);
+	}
+
+}
